b7a6746115803a9d0f97c349922ff2c8cec778de
Sten Kaal <stenkaal@hotmail.com>
2025-06-20 00:23:31 +0200

    fixed centered text on mobile

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
                 }}
               />
             </div>
-            <div className="absolute inset-0 z-10 flex flex-col items-start justify-center px-8 text-white max-w-[1100px] top-1/2">
+            <div className="absolute inset-0 z-10 flex flex-col items-start justify-center px-8 text-white max-w-[1100px] lg:top-1/2">
               <h1 className="font-sans font-black text-5xl sm:text-6xl md:text-7xl lg:text-8xl">
                 Finding <b className="bg-white px-2 text-black">solutions</b> for{" "}
                 <u>tomorrows</u> problems.
