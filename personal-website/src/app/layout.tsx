9ed11af5200a3e231084fb8a095080bdbdb721aa
Sten Kaal <stenkaal@hotmail.com>
2025-06-20 00:16:46 +0200

    Testing

--- personal-website/src/app/layout.tsx
+++ personal-website/src/app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother } from 'gsap/ScrollSmoother';
-import { useEffect, useRef } from "react";
 
-if (typeof window !== "undefined") {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
